8b2b341ce3b95495d945e9b764260a6350daf07d
Evisceration <eviscerationls@gmail.com>
2014-02-11 20:04:31 +0100

    performance: add time in state to first screen
    Change-Id: I8f53bee2fddb3509b2436aedf85c3e2fbb05bc40

--- src/org/namelessrom/devicecontrol/fragments/performance/PerformanceHelpFragment.java
+++ src/org/namelessrom/devicecontrol/fragments/performance/PerformanceHelpFragment.java
@@ -18,33 +18,183 @@
 package org.namelessrom.devicecontrol.fragments.performance;
 
 import android.app.Fragment;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import org.namelessrom.devicecontrol.R;
+import org.namelessrom.devicecontrol.utils.CPUStateMonitor;
 import org.namelessrom.devicecontrol.utils.constants.DeviceConstants;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by alex on 18.12.13.
  */
 public class PerformanceHelpFragment extends Fragment implements DeviceConstants {
 
+    private LinearLayout mStatesView;
+    private TextView mAdditionalStates;
+    private TextView mTotalStateTime;
+    private TextView mHeaderAdditionalStates;
+    private TextView mHeaderTotalStateTime;
+    private TextView mStatesWarning;
+    private boolean mUpdatingData = false;
+
+    private CPUStateMonitor monitor = new CPUStateMonitor();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_help,
+        View view = inflater.inflate(R.layout.fragment_performance,
                 container, false);
 
-        TextView tvHelp = (TextView) view.findViewById(R.id.help_textview);
-        tvHelp.setText(R.string.app_performance_help);
-
-        ImageView ivHelp = (ImageView) view.findViewById(R.id.help_imageview);
-        ivHelp.setImageResource(R.mipmap.ic_launcher);
+        mStatesView = (LinearLayout) view.findViewById(R.id.ui_states_view);
+        mAdditionalStates = (TextView) view.findViewById(R.id.ui_additional_states);
+        mHeaderAdditionalStates = (TextView) view.findViewById(R.id.ui_header_additional_states);
+        mHeaderTotalStateTime = (TextView) view.findViewById(R.id.ui_header_total_state_time);
+        mStatesWarning = (TextView) view.findViewById(R.id.ui_states_warning);
+        mTotalStateTime = (TextView) view.findViewById(R.id.ui_total_state_time);
 
         return view;
     }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean("updatingData", mUpdatingData);
+    }
+
+    @Override
+    public void onResume() {
+        refreshData();
+        super.onResume();
+    }
+
+    public void updateView() {
+        mStatesView.removeAllViews();
+        List<String> extraStates = new ArrayList<String>();
+        for (CPUStateMonitor.CpuState state : monitor.getStates()) {
+            if (state.duration > 0) {
+                generateStateRow(state, mStatesView);
+            } else {
+                if (state.freq == 0) {
+                    extraStates.add(getString(R.string.deep_sleep));
+                } else {
+                    extraStates.add(state.freq / 1000 + " MHz");
+                }
+            }
+        }
+
+        if (monitor.getStates().size() == 0) {
+            mStatesWarning.setVisibility(View.VISIBLE);
+            mHeaderTotalStateTime.setVisibility(View.GONE);
+            mTotalStateTime.setVisibility(View.GONE);
+            mStatesView.setVisibility(View.GONE);
+        }
+
+        long totTime = monitor.getTotalStateTime() / 100;
+        mTotalStateTime.setText(toString(totTime));
+
+        if (extraStates.size() > 0) {
+            int n = 0;
+            String str = "";
+
+            for (String s : extraStates) {
+                if (n++ > 0)
+                    str += ", ";
+                str += s;
+            }
+
+            mAdditionalStates.setVisibility(View.VISIBLE);
+            mHeaderAdditionalStates.setVisibility(View.VISIBLE);
+            mAdditionalStates.setText(str);
+        } else {
+            mAdditionalStates.setVisibility(View.GONE);
+            mHeaderAdditionalStates.setVisibility(View.GONE);
+        }
+    }
+
+    public void refreshData() {
+        if (!mUpdatingData) {
+            new RefreshStateDataTask().execute((Void) null);
+        }
+    }
+
+    private static String toString(long tSec) {
+        long h = (long) Math.floor(tSec / (60 * 60));
+        long m = (long) Math.floor((tSec - h * 60 * 60) / 60);
+        long s = tSec % 60;
+        String sDur;
+        sDur = h + ":";
+        if (m < 10)
+            sDur += "0";
+        sDur += m + ":";
+        if (s < 10)
+            sDur += "0";
+        sDur += s;
+
+        return sDur;
+    }
+
+    private View generateStateRow(CPUStateMonitor.CpuState state, ViewGroup parent) {
+
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+        LinearLayout view = (LinearLayout) inflater.inflate(R.layout.state_row, parent, false);
+
+        float per = (float) state.duration * 100 / monitor.getTotalStateTime();
+        String sPer = (int) per + "%";
+
+        String sFreq;
+        if (state.freq == 0) {
+            sFreq = getString(R.string.deep_sleep);
+        } else {
+            sFreq = state.freq / 1000 + " MHz";
+        }
+
+        long tSec = state.duration / 100;
+        String sDur = toString(tSec);
+
+        TextView freqText = (TextView) view.findViewById(R.id.ui_freq_text);
+        TextView durText = (TextView) view.findViewById(R.id.ui_duration_text);
+        TextView perText = (TextView) view.findViewById(R.id.ui_percentage_text);
+        ProgressBar bar = (ProgressBar) view.findViewById(R.id.ui_bar);
+
+        freqText.setText(sFreq);
+        perText.setText(sPer);
+        durText.setText(sDur);
+        bar.setProgress((int) per);
+
+        parent.addView(view);
+        return view;
+    }
+
+    protected class RefreshStateDataTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... v) {
+            try {
+                monitor.updateStates();
+            } catch (CPUStateMonitor.CPUStateMonitorException e) {
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            mUpdatingData = true;
+        }
+
+        @Override
+        protected void onPostExecute(Void v) {
+            updateView();
+            mUpdatingData = false;
+        }
+    }
+
 }
---
+++ src/org/namelessrom/devicecontrol/utils/CPUStateMonitor.java
@@ -0,0 +1,153 @@
+/*
+ * Performance Control - An Android CPU Control application
+ * Copyright (C) Brandon Valosek, 2011 <bvalosek@gmail.com>
+ * Copyright (C) Modified by 2012 James Roberts
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.namelessrom.devicecontrol.utils;
+
+import android.os.SystemClock;
+
+import org.namelessrom.devicecontrol.utils.constants.DeviceConstants;
+import org.namelessrom.devicecontrol.utils.helpers.CpuUtils;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CPUStateMonitor implements DeviceConstants {
+
+    private List<CpuState> mStates = new ArrayList<CpuState>();
+    private Map<Integer, Long> mOffsets = new HashMap<Integer, Long>();
+
+    public class CPUStateMonitorException extends Exception {
+        public CPUStateMonitorException(String s) {
+            super(s);
+        }
+    }
+
+    public class CpuState implements Comparable<CpuState> {
+        public CpuState(int a, long b) {
+            freq = a;
+            duration = b;
+        }
+
+        public int freq = 0;
+        public long duration = 0;
+
+        public int compareTo(CpuState state) {
+            Integer a = freq;
+            Integer b = state.freq;
+            return a.compareTo(b);
+        }
+    }
+
+    public List<CpuState> getStates() {
+        List<CpuState> states = new ArrayList<CpuState>();
+
+        for (CpuState state : mStates) {
+            long duration = state.duration;
+            if (mOffsets.containsKey(state.freq)) {
+                long offset = mOffsets.get(state.freq);
+                if (offset <= duration) {
+                    duration -= offset;
+                } else {
+                    mOffsets.clear();
+                    return getStates();
+                }
+            }
+            states.add(new CpuState(state.freq, duration));
+        }
+        return states;
+    }
+
+    public long getTotalStateTime() {
+        long sum = 0;
+        long offset = 0;
+
+        for (CpuState state : mStates) {
+            sum += state.duration;
+        }
+
+        for (Map.Entry<Integer, Long> entry : mOffsets.entrySet()) {
+            offset += entry.getValue();
+        }
+        return sum - offset;
+    }
+
+    public Map<Integer, Long> getOffsets() {
+        return mOffsets;
+    }
+
+    public void setOffsets(Map<Integer, Long> offsets) {
+        mOffsets = offsets;
+    }
+
+    public void setOffsets() throws CPUStateMonitorException {
+        mOffsets.clear();
+        updateStates();
+
+        for (CpuState state : mStates) {
+            mOffsets.put(state.freq, state.duration);
+        }
+    }
+
+    public void removeOffsets() {
+        mOffsets.clear();
+    }
+
+    public List<CpuState> updateStates() throws CPUStateMonitorException {
+        try {
+            InputStream is = new FileInputStream(CpuUtils.FREQ_TIME_IN_STATE_PATH);
+            InputStreamReader ir = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(ir);
+            mStates.clear();
+            readInStates(br);
+            is.close();
+        } catch (IOException e) {
+            throw new CPUStateMonitorException(
+                    "Problem opening time-in-states file");
+        }
+
+        long sleepTime = (SystemClock.elapsedRealtime() - SystemClock.uptimeMillis()) / 10;
+        mStates.add(new CpuState(0, sleepTime));
+
+        Collections.sort(mStates, Collections.reverseOrder());
+
+        return mStates;
+    }
+
+    private void readInStates(BufferedReader br)
+            throws CPUStateMonitorException {
+        try {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] nums = line.split(" ");
+                mStates.add(new CpuState(Integer.parseInt(nums[0]), Long.parseLong(nums[1])));
+            }
+        } catch (IOException e) {
+            throw new CPUStateMonitorException(
+                    "Problem processing time-in-states file");
+        }
+    }
+}
