9a94d082453341fa0ba302132f69e835ca1ceec5
Evisceration <eviscerationls@gmail.com>
2014-03-11 18:24:54 +0100

    version: 0.2.10
    also bring in gradle
    
    Change-Id: I7805da0c1cf98e5f59fa8fb132ef1c39d9c8745d

--- Android.mk
+++ Android.mk
@@ -1,19 +1,3 @@
-LOCAL_PATH:= $(call my-dir)
+LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
-
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13 acra
-
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
-
-LOCAL_ASSET_DIR += packages/apps/DeviceControl/assets
-
-LOCAL_PACKAGE_NAME := DeviceControl
-LOCAL_CERTIFICATE := platform
-LOCAL_PRIVILEGED_MODULE := true
-
-include $(BUILD_PACKAGE)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
+include $(LOCAL_PATH)/app/src/main/Android.mk
---
+++ app/src/main/Android.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13 acra
+
+LOCAL_SRC_FILES     := $(call all-java-files-under,java)
+LOCAL_RESOURCE_DIR  := $(LOCAL_PATH)/res
+LOCAL_ASSET_DIR     := $(LOCAL_PATH)/assets
+
+LOCAL_PACKAGE_NAME := DeviceControl
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+
+include $(BUILD_PACKAGE)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
