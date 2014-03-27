4be37e0db1e5837b63e3d540c6e540e5a3c9f6f1
Evisceration <eviscerationls@gmail.com>
2014-03-27 23:47:03 +0100

    DC: add clean and create scripts to make my life easier
    Change-Id: I057d49dcc4903d974a16b05a6b22cc0f7df9c70d

---
+++ clean_create_package.sh
@@ -0,0 +1,5 @@
+echo "===== Cleaning ====="
+./gradlew clean
+echo "===== Building ====="
+./gradlew assembleRelease
+echo "=====   DONE   ====="
---
+++ create_package.sh
@@ -0,0 +1,3 @@
+echo "===== Building ====="
+./gradlew assembleRelease
+echo "=====   DONE   ====="
