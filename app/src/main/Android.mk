1d3265466997e30d6c9813b6ae08ffcc17345d65
Evisceration <eviscerationls@gmail.com>
2014-03-28 08:24:37 +0100

    DC: replace prebuilt otto library with sources, make it FOSS
    Change-Id: Ia6523b2ca8b7483e1d2477781ff3dc435a033cd2

--- app/src/main/Android.mk
+++ app/src/main/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13 acra otto
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13 acra
 
 LOCAL_SRC_FILES     := $(call all-java-files-under,java)
 LOCAL_ASSET_DIR     := $(LOCAL_PATH)/assets
