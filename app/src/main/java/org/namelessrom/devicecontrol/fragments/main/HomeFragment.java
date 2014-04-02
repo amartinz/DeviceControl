414b84e5edb50a916520f44f4aefdf99f6efeb4a
Evisceration <eviscerationls@gmail.com>
2014-04-03 01:25:29 +0200

    DC: make home fragment always selectable in menu, other cleanups
    Change-Id: Ibad1e028c9479594c568d4219924ce81fd537ea5

--- app/src/main/java/org/namelessrom/devicecontrol/fragments/main/HomeFragment.java
+++ app/src/main/java/org/namelessrom/devicecontrol/fragments/main/HomeFragment.java
@@ -30,13 +30,13 @@ import org.namelessrom.devicecontrol.activities.MainActivity;
 import org.namelessrom.devicecontrol.utils.constants.DeviceConstants;
 import org.namelessrom.devicecontrol.widgets.AttachFragment;
 
-public class InformationFragment extends AttachFragment implements DeviceConstants {
+public class HomeFragment extends AttachFragment implements DeviceConstants {
 
     public static final int ID = 0;
 
     @Override
     public void onAttach(Activity activity) {
-        super.onAttach(activity, InformationFragment.ID);
+        super.onAttach(activity, HomeFragment.ID);
     }
 
     @Override
@@ -48,8 +48,7 @@ public class InformationFragment extends AttachFragment implements DeviceConstan
         final String appname = getString(R.string.app_name);
 
         final TextView tvHelp = (TextView) view.findViewById(R.id.help_textview);
-        tvHelp.setText(getString(R.string.app_information_help,
-                appname, appname, appname));
+        tvHelp.setText(getString(R.string.app_information_help, appname, appname));
 
         final ImageView ivHelp = (ImageView) view.findViewById(R.id.help_imageview);
         ivHelp.setImageResource(R.mipmap.ic_launcher);
