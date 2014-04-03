b0a06b67996dbf57dd30f770f18eedd998b10b33
Evisceration <eviscerationls@gmail.com>
2014-04-03 10:21:02 +0200

    DC: extras: control mpdecision
    Change-Id: Ie27e98c28f76c9734a7cc38cf4d4ca5e233e562b

--- app/src/main/java/org/namelessrom/devicecontrol/events/PerformanceExtrasFragmentEvent.java
+++ app/src/main/java/org/namelessrom/devicecontrol/events/PerformanceExtrasFragmentEvent.java
@@ -3,11 +3,16 @@ package org.namelessrom.devicecontrol.events;
 public class PerformanceExtrasFragmentEvent {
 
     private final boolean mIsForceHighEndGfx;
+    private final boolean mIsMpDecisionRunning;
 
-    public PerformanceExtrasFragmentEvent(final boolean isForceHighEndGfx) {
+    public PerformanceExtrasFragmentEvent(final boolean isForceHighEndGfx,
+            final boolean isMpDecisionRunning) {
         mIsForceHighEndGfx = isForceHighEndGfx;
+        mIsMpDecisionRunning = isMpDecisionRunning;
     }
 
     public boolean isForceHighEndGfx() { return mIsForceHighEndGfx; }
+
+    public boolean isMpDecisionRunning() { return mIsMpDecisionRunning; }
 
 }
