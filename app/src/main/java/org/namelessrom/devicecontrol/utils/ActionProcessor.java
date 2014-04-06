799444e029f5c090a807848bde05d16669a081e1
Evisceration <eviscerationls@gmail.com>
2014-04-06 22:04:50 +0200

    DC: add a real tasker
    for now we only offered automatic fstrimming at specified intervals.
    this time, we add a more "serious" tasker.
    
    it lets you add, delete, enable and disable tasks.
    for now we add a screen off and on category as event, when the actions
    should get executed.
    as actions we add changing cpu min and max frequency, cpu governor and
    i/o scheduler.
    
    more to come!
    
    Change-Id: I18d0133017dfd894306f722dc390ea329bf25a56

---
+++ app/src/main/java/org/namelessrom/devicecontrol/utils/ActionProcessor.java
@@ -0,0 +1,76 @@
+package org.namelessrom.devicecontrol.utils;
+
+import org.namelessrom.devicecontrol.database.TaskerItem;
+import org.namelessrom.devicecontrol.utils.constants.PerformanceConstants;
+
+import static org.namelessrom.devicecontrol.Application.logDebug;
+
+public class ActionProcessor implements PerformanceConstants {
+
+    public static final String ACTION_CPU_FREQUENCY_MAX = "cpu_frequency_max";
+    public static final String ACTION_CPU_FREQUENCY_MIN = "cpu_frequency_min";
+    public static final String ACTION_CPU_GOVERNOR      = "cpu_governor";
+    public static final String ACTION_IO_SCHEDULER      = "io_scheduler";
+
+    public static final String[] CATEGORIES     =
+            {TaskerItem.CATEGORY_SCREEN_ON, TaskerItem.CATEGORY_SCREEN_OFF};
+    public static final String[] SCREEN_ACTIONS =
+            {
+                    ACTION_CPU_FREQUENCY_MAX, ACTION_CPU_FREQUENCY_MIN, ACTION_CPU_GOVERNOR,
+                    ACTION_IO_SCHEDULER
+            };
+
+    public static void processAction(final String action, final String value) {
+        final StringBuilder sb = new StringBuilder();
+        //------------------------------------------------------------------------------------------
+        // CPU
+        //------------------------------------------------------------------------------------------
+        final int cpu = CpuUtils.getNumOfCpus();
+        String path;
+
+        if (ACTION_CPU_FREQUENCY_MAX.equals(action)) {
+            for (int i = 0; i < cpu; i++) {
+                sb.append(onlineCpu(i));
+                path = CpuUtils.getMaxCpuFrequencyPath(i);
+                if (!value.isEmpty()) {
+                    sb.append(Utils.getWriteCommand(path, value));
+                }
+            }
+        } else if (ACTION_CPU_FREQUENCY_MIN.equals(action)) {
+            for (int i = 0; i < cpu; i++) {
+                sb.append(onlineCpu(i));
+                path = CpuUtils.getMinCpuFrequencyPath(i);
+                if (!value.isEmpty()) {
+                    sb.append(Utils.getWriteCommand(path, value));
+                }
+            }
+        } else if (ACTION_CPU_GOVERNOR.equals(action)) {
+            for (int i = 0; i < cpu; i++) {
+                sb.append(onlineCpu(i));
+                path = CpuUtils.getGovernorPath(i);
+                if (!value.isEmpty()) {
+                    sb.append(Utils.getWriteCommand(path, value));
+                }
+            }
+        } else if (ACTION_IO_SCHEDULER.equals(action)) {
+            for (final String schedulerPath : IO_SCHEDULER_PATH) {
+                sb.append(Utils.getWriteCommand(schedulerPath, value));
+            }
+        }
+
+        final String cmd = sb.toString();
+        logDebug("Performing Action: " + cmd);
+        Utils.runRootCommand(cmd);
+    }
+
+    private static String onlineCpu(int cpu) {
+        final StringBuilder sb = new StringBuilder();
+        final String pathOnline = CpuUtils.getOnlinePath(cpu);
+        if (!pathOnline.isEmpty()) {
+            sb.append(Utils.getWriteCommand(pathOnline, "0"));
+            sb.append(Utils.getWriteCommand(pathOnline, "1"));
+        }
+        return sb.toString();
+    }
+
+}
