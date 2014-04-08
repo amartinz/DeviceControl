e9e479b35447774ed79df2fff06e343e45210b14
Evisceration <eviscerationls@gmail.com>
2014-04-09 01:27:55 +0200

    ...
    Change-Id: Icd5af34579f6dbf1238364b628afb40fdce7710b

--- app/src/main/java/org/namelessrom/devicecontrol/database/DatabaseHandler.java
+++ app/src/main/java/org/namelessrom/devicecontrol/database/DatabaseHandler.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
 
-    private static final int    DATABASE_VERSION = 3;
+    private static final int    DATABASE_VERSION = 4;
     private static final String DATABASE_NAME    = "DeviceControl.db";
 
     private static final String KEY_ID       = "id";
@@ -22,12 +22,17 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     private static final String KEY_ENABLED  = "enabled";
 
     public static final String TABLE_BOOTUP = "boot_up";
+    public static final String TABLE_DC     = "devicecontrol";
     public static final String TABLE_TASKER = "tasker";
 
     private static final String CREATE_BOOTUP_TABLE = "CREATE TABLE " + TABLE_BOOTUP + '('
             + KEY_ID + " INTEGER PRIMARY KEY," + KEY_CATEGORY + " TEXT," + KEY_NAME + " TEXT,"
             + KEY_FILENAME + " TEXT," + KEY_VALUE + " TEXT)";
     private static final String DROP_BOOTUP_TABLE   = "DROP TABLE IF EXISTS " + TABLE_BOOTUP;
+
+    private static final String CREATE_DEVICE_CONTROL_TABLE = "CREATE TABLE " + TABLE_DC + '('
+            + KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT," + KEY_VALUE + " TEXT)";
+    private static final String DROP_DEVICE_CONTROL_TABLE   = "DROP TABLE IF EXISTS " + TABLE_DC;
 
     private static final String CREATE_TASKER_TABLE = "CREATE TABLE " + TABLE_TASKER + '('
             + KEY_ID + " INTEGER PRIMARY KEY," + KEY_CATEGORY + " TEXT," + KEY_NAME + " TEXT,"
@@ -50,6 +55,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     @Override
     public void onCreate(final SQLiteDatabase db) {
         db.execSQL(CREATE_BOOTUP_TABLE);
+        db.execSQL(CREATE_DEVICE_CONTROL_TABLE);
         db.execSQL(CREATE_TASKER_TABLE);
     }
 
@@ -70,11 +76,47 @@ public class DatabaseHandler extends SQLiteOpenHelper {
             db.execSQL(CREATE_TASKER_TABLE);
             currentVersion = 3;
         }
+
+        if (currentVersion < 4) {
+            db.execSQL(DROP_DEVICE_CONTROL_TABLE);
+            db.execSQL(CREATE_DEVICE_CONTROL_TABLE);
+            currentVersion = 4;
+        }
     }
 
     //==============================================================================================
     // All CRUD(Create, Read, Update, Delete) Operations
     //==============================================================================================
+
+    public String getValueByName(final String name, final String tableName) {
+        final SQLiteDatabase db = getReadableDatabase();
+
+        if (db == null) return null;
+
+        final Cursor cursor = db.query(tableName, new String[]{KEY_VALUE}, KEY_NAME + "=?",
+                new String[]{name}, null, null, null, null
+        );
+        if (cursor != null) { cursor.moveToFirst(); }
+        if (cursor == null) return null;
+
+        return cursor.getCount() <= 0 ? null : cursor.getString(cursor.getColumnIndex(KEY_VALUE));
+    }
+
+    public boolean insertOrUpdate(final String name, final String value, final String tableName) {
+        final SQLiteDatabase db = getReadableDatabase();
+
+        if (db == null) return false;
+
+        final ContentValues values = new ContentValues();
+        values.put(KEY_NAME, name);
+        values.put(KEY_VALUE, value);
+
+        db.delete(tableName, KEY_NAME + " = ?", new String[]{name});
+        db.insert(tableName, null, values);
+        db.close();
+
+        return true;
+    }
 
     public boolean addItem(final DataItem item, final String tableName) {
         final SQLiteDatabase db = getWritableDatabase();
