91eb5f602fd9539a916006186d5f880a6474fbcc
Evisceration <eviscerationls@gmail.com>
2014-04-20 19:04:01 +0200

    DC: move IAB to proprietary repo
    Change-Id: Ic62406d7a603c7cb10202ce08fef3bccdb999ba7

--- app/src/main/Android.mk
+++ app/src/main/Android.mk
@@ -5,12 +5,22 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13 acra
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13 acra play
 
-LOCAL_SRC_FILES     := $(call all-java-files-under,java) \
-        aidl/com/android/vending/billing/IInAppBillingService.aidl
+LOCAL_SRC_FILES     := \
+        $(call all-java-files-under,java) \
+        aidl/com/android/vending/billing/IInAppBillingService.aidl \
+        $(call all-java-files-under,../../../proprietary/src/main/java)
 LOCAL_ASSET_DIR     := $(LOCAL_PATH)/assets
-LOCAL_RESOURCE_DIR  := $(LOCAL_PATH)/res
+
+# Include res dir from chips
+google_play_dir := \
+        ../../../../../../external/google/google_play_services/libproject/google-play-services_lib/res
+res_dir := $(google_play_dir) res
+
+LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dir))
+LOCAL_AAPT_FLAGS := --auto-add-overlay
+LOCAL_AAPT_FLAGS += --extra-packages com.google.android.gms
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
@@ -19,5 +29,11 @@ LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
 include $(BUILD_PACKAGE)
+
+include $(CLEAR_VARS)
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
+        play:../../../../../../external/google/google_play_services/libproject/google-play-services_lib/libs/google-play-services.jar
+
+include $(BUILD_MULTI_PREBUILT)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
