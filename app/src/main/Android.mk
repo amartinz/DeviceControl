6d36b916e5cbfc591c89f7f49efc59f44e62afbf
Evisceration <eviscerationls@gmail.com>
2014-04-20 20:10:39 +0200

    DC: more changes for proprietary repository
    Change-Id: Id6e66a7882d3ae74e9ec5ab904f8bdfb8cb86176

--- app/src/main/Android.mk
+++ app/src/main/Android.mk
@@ -6,6 +6,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
+        android-support-v7-appcompat \
         android-support-v7-mediarouter \
         android-support-v13 \
         acra \
@@ -17,10 +18,11 @@ LOCAL_SRC_FILES     := \
         $(call all-java-files-under,../../../proprietary/src/main/java)
 LOCAL_ASSET_DIR     := $(LOCAL_PATH)/assets
 
-# Include res dir from chips
 google_play_dir := \
         ../../../../../../external/google/google_play_services/libproject/google-play-services_lib/res
-res_dir := $(google_play_dir) res
+proprietary_dir := \
+        ../../../proprietary/src/main/res
+res_dir := $(google_play_dir) $(proprietary_dir) res
 
 LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dir))
 LOCAL_AAPT_FLAGS := --auto-add-overlay
