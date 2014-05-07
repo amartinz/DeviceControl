1b085bf591c8c2987afe8db4c870ad2d4ac5277f
Evisceration <eviscerationls@gmail.com>
2014-05-07 10:07:38 +0200

    DC: add API with sample (WIP)
    apps which want to make use of the API need permission to use it.
    permission: org.namelessrom.devicecontrol.API
    
    Change-Id: I984c90b7a54e03b05ca1ac4a5d1aeda61c8b00e7

---
+++ Sample/src/main/java/org/namelessrom/devicecontrol/sample/events/ServiceConnectedEvent.java
@@ -0,0 +1,7 @@
+package org.namelessrom.devicecontrol.sample.events;
+
+/**
+ * An event for Otto.
+ * Used when an service connected
+ */
+public class ServiceConnectedEvent {}
---
+++ Sample/src/main/java/org/namelessrom/devicecontrol/sample/events/ServiceDisconnectedEvent.java
@@ -0,0 +1,7 @@
+package org.namelessrom.devicecontrol.sample.events;
+
+/**
+ * An event for Otto.
+ * Used when an service disconnected
+ */
+public class ServiceDisconnectedEvent {}
