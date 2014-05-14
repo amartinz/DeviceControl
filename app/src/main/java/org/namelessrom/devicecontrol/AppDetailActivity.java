1c6c27c2c8157fc0f920cf817ebdeb1182c5f065
Evisceration <eviscerationls@gmail.com>
2014-05-14 15:48:34 +0200

    DC: add butterknife and holograph, add graphs to app detail screen
    Change-Id: Id9605a12e0649659c7a65a9636c24de61ce94d65

--- app/src/main/java/org/namelessrom/devicecontrol/AppDetailActivity.java
+++ app/src/main/java/org/namelessrom/devicecontrol/AppDetailActivity.java
@@ -9,6 +9,7 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageStats;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -20,6 +21,8 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.echo.holographlibrary.Bar;
+import com.echo.holographlibrary.BarGraph;
 import com.squareup.otto.Subscribe;
 import com.stericson.roottools.RootTools;
 import com.stericson.roottools.execution.CommandCapture;
@@ -29,6 +32,12 @@ import org.namelessrom.devicecontrol.objects.AppItem;
 import org.namelessrom.devicecontrol.utils.AppHelper;
 import org.namelessrom.devicecontrol.utils.providers.BusProvider;
 
+import java.util.ArrayList;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+
 import static org.namelessrom.devicecontrol.Application.logDebug;
 
 public class AppDetailActivity extends Activity {
@@ -36,12 +45,17 @@ public class AppDetailActivity extends Activity {
     private final Handler        mHandler = new Handler();
     private final PackageManager mPm      = Application.getPm();
 
-    private TextView     mStatus;
-    private Button       mDisabler;
-    private Button       mKillApp;
-    private LinearLayout mCacheInfo;
-    private Button       mClearData;
-    private Button       mClearCache;
+    @InjectView(R.id.app_icon)    ImageView appIcon;
+    @InjectView(R.id.app_label)   TextView  appLabel;
+    @InjectView(R.id.app_package) TextView  appPackage;
+
+    @InjectView(R.id.app_status)               TextView     mStatus;
+    @InjectView(R.id.app_kill)                 Button       mKillApp;
+    @InjectView(R.id.app_disabler)             Button       mDisabler;
+    @InjectView(R.id.app_cache_graph)          BarGraph     mCacheGraph;
+    @InjectView(R.id.app_cache_info_container) LinearLayout mCacheInfo;
+    @InjectView(R.id.app_data_clear)           Button       mClearData;
+    @InjectView(R.id.app_cache_clear)          Button       mClearCache;
 
     private AppItem mAppItem;
 
@@ -61,6 +75,7 @@ public class AppDetailActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_details);
+        ButterKnife.inject(this);
 
         String tmp;
         PackageInfo pkgInfo;
@@ -81,16 +96,10 @@ public class AppDetailActivity extends Activity {
 
         setupActionBar();
 
-        final ImageView appIcon = (ImageView) findViewById(R.id.app_icon);
         appIcon.setImageDrawable(mAppItem.getIcon());
-
-        final TextView appLabel = (TextView) findViewById(R.id.app_label);
         appLabel.setText(mAppItem.getLabel());
-
-        final TextView appPackage = (TextView) findViewById(R.id.app_package);
         appPackage.setText(mAppItem.getPackageName());
 
-        mStatus = (TextView) findViewById(R.id.app_status);
         if (mAppItem.isSystemApp()) {
             tmp = getString(R.string.app_system, mAppItem.getLabel());
             mStatus.setTextColor(getResources().getColor(R.color.red_middle));
@@ -99,58 +108,42 @@ public class AppDetailActivity extends Activity {
         }
         mStatus.setText(Html.fromHtml(tmp));
 
-        mKillApp = (Button) findViewById(R.id.app_kill);
         if (!AppHelper.isAppRunning(this, mAppItem.getPackageName())) {
             mKillApp.setEnabled(false);
         }
-        mKillApp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mKillApp.setEnabled(false);
-                AppHelper.killApp(mAppItem.getPackageName());
-                mHandler.postDelayed(mKillRunnable, 500);
-            }
-        });
 
-        mDisabler = (Button) findViewById(R.id.app_disabler);
         if (mAppItem.getPackageName().contains("org.namelessrom")) {
             mDisabler.setEnabled(false);
         }
         mDisabler.setText(mAppItem.isEnabled() ? R.string.disable : R.string.enable);
-        mDisabler.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showConfirmationDialog();
-            }
-        });
-
-        mCacheInfo = (LinearLayout) findViewById(R.id.app_cache_info_container);
-
-        mClearData = (Button) findViewById(R.id.app_data_clear);
-        mClearData.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mClearCache.setEnabled(false);
-                mClearData.setEnabled(false);
-                AppHelper.clearData(mAppItem.getPackageName());
-                mHandler.postDelayed(mClearRunnable, 500);
-            }
-        });
-
-        mClearCache = (Button) findViewById(R.id.app_cache_clear);
-        mClearCache.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mClearCache.setEnabled(false);
-                mClearData.setEnabled(false);
-                AppHelper.clearCache(mAppItem.getPackageName());
-                mHandler.postDelayed(mClearRunnable, 500);
-            }
-        });
 
         try {
             AppHelper.getSize(mPm, mAppItem.getPackageName());
         } catch (Exception e) { logDebug("AppHelper.getSize(): " + e); }
+    }
+
+    @OnClick(R.id.app_kill) void killApp() {
+        mKillApp.setEnabled(false);
+        AppHelper.killApp(mAppItem.getPackageName());
+        mHandler.postDelayed(mKillRunnable, 500);
+    }
+
+    @OnClick(R.id.app_disabler) void disableApp() {
+        showConfirmationDialog();
+    }
+
+    @OnClick(R.id.app_data_clear) void clearAppData() {
+        mClearCache.setEnabled(false);
+        mClearData.setEnabled(false);
+        AppHelper.clearData(mAppItem.getPackageName());
+        mHandler.postDelayed(mClearRunnable, 500);
+    }
+
+    @OnClick(R.id.app_cache_clear) void cleanAppCache() {
+        mClearCache.setEnabled(false);
+        mClearData.setEnabled(false);
+        AppHelper.clearCache(mAppItem.getPackageName());
+        mHandler.postDelayed(mClearRunnable, 500);
     }
 
     private void setupActionBar() {
@@ -229,7 +222,8 @@ public class AppDetailActivity extends Activity {
             mCacheInfo.addView(addCacheWidget(R.string.total,
                     AppHelper.convertSize(packageStats.codeSize + packageStats.dataSize
                             + packageStats.externalCodeSize + packageStats.externalDataSize
-                            + packageStats.externalMediaSize + packageStats.externalObbSize)
+                            + packageStats.externalMediaSize + packageStats.externalObbSize
+                            + packageStats.externalCacheSize)
             ));
             mCacheInfo.addView(addCacheWidget(R.string.app,
                     AppHelper.convertSize(packageStats.codeSize)));
@@ -245,15 +239,55 @@ public class AppDetailActivity extends Activity {
                     AppHelper.convertSize(packageStats.externalObbSize)));
             mCacheInfo.addView(addCacheWidget(R.string.cache,
                     AppHelper.convertSize(packageStats.cacheSize)));
+            mCacheInfo.addView(addCacheWidget(R.string.ext_cache,
+                    AppHelper.convertSize(packageStats.externalCacheSize)));
         }
+
+        if (mCacheGraph != null) {
+            final ArrayList<Bar> barList = new ArrayList<Bar>();
+            final Resources r = getResources();
+            // Total -------------------------------------------------------------------------------
+            String text = getString(R.string.total);
+            barList.add(createBar(packageStats.codeSize + packageStats.dataSize
+                            + packageStats.externalCodeSize + packageStats.externalDataSize
+                            + packageStats.externalMediaSize + packageStats.externalObbSize,
+                    text, r.getColor(R.color.greenish)
+            ));
+            // App ---------------------------------------------------------------------------------
+            text = getString(R.string.app);
+            barList.add(createBar(packageStats.codeSize + packageStats.externalCodeSize,
+                    text, r.getColor(R.color.red_middle)));
+            // Data --------------------------------------------------------------------------------
+            text = getString(R.string.data);
+            barList.add(createBar(packageStats.dataSize + packageStats.externalDataSize,
+                    text, r.getColor(R.color.orange)));
+            // External ------------------------------------------------------------------------
+            text = getString(R.string.ext);
+            barList.add(createBar(packageStats.externalMediaSize + packageStats.externalObbSize,
+                    text, r.getColor(R.color.blueish)));
+            // Cache -------------------------------------------------------------------------------
+            text = getString(R.string.cache);
+            barList.add(createBar(packageStats.cacheSize + packageStats.externalCacheSize,
+                    text, r.getColor(R.color.review_green)));
+            mCacheGraph.setBars(barList);
+        }
+    }
+
+    private Bar createBar(final long value, final String text, final int color) {
+        final Bar bar = new Bar();
+        bar.setName(text);
+        bar.setValue(value);
+        bar.setValueString(AppHelper.convertSize(value));
+        bar.setColor(color);
+        return bar;
     }
 
     private View addCacheWidget(final int txtId, final String text) {
         final View v = ((LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE))
                 .inflate(R.layout.widget_app_cache, mCacheInfo, false);
 
-        final TextView tvLeft = (TextView) v.findViewById(R.id.widget_app_cache_left);
-        final TextView tvRight = (TextView) v.findViewById(R.id.widget_app_cache_right);
+        final TextView tvLeft = ButterKnife.findById(v, R.id.widget_app_cache_left);
+        final TextView tvRight = ButterKnife.findById(v, R.id.widget_app_cache_right);
 
         tvLeft.setText(getString(txtId) + ':');
         tvRight.setText(text);
