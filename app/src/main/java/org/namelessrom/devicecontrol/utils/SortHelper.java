73cf7d44d12445bff081d9d534bf0bb1e60f1cf5
Evisceration <eviscerationls@gmail.com>
2014-05-27 14:03:07 +0200

    server tests (DO NOT MERGE)
    Change-Id: I8b402c350d826f482d54bc82b3d389df0540f399

--- app/src/main/java/org/namelessrom/devicecontrol/utils/SortHelper.java
+++ app/src/main/java/org/namelessrom/devicecontrol/utils/SortHelper.java
@@ -2,6 +2,7 @@ package org.namelessrom.devicecontrol.utils;
 
 import org.namelessrom.devicecontrol.objects.AppItem;
 
+import java.io.File;
 import java.text.Collator;
 import java.util.Comparator;
 
@@ -10,12 +11,19 @@ import java.util.Comparator;
  */
 public class SortHelper {
 
-    public final static Comparator<AppItem> sAppcomparator = new Comparator<AppItem>() {
-        public final int compare(AppItem a, AppItem b) {
+    public static final Comparator<AppItem> sAppComparator = new Comparator<AppItem>() {
+        public final int compare(final AppItem a, final AppItem b) {
             return collator.compare(a.getLabel(), b.getLabel());
         }
 
         private final Collator collator = Collator.getInstance();
+    };
+
+    public static final Comparator<File> sFileComparator = new Comparator<File>() {
+        @Override
+        public int compare(final File lhs, final File rhs) {
+            return lhs.getName().toLowerCase().compareTo(rhs.getName().toLowerCase());
+        }
     };
 
 }
