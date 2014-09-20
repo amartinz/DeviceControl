5096724c1015bc1dab78d2ef5d19e60a583cf70a
Evisceration <eviscerationls@gmail.com>
2014-09-20 17:20:42 +0200

    add more device, cpu and kernel informations
    Change-Id: Ia4783607a6105229750abe533a41104621bc6a32

--- app/src/main/java/org/namelessrom/devicecontrol/fragments/device/DeviceInformationFragment.java
+++ app/src/main/java/org/namelessrom/devicecontrol/fragments/device/DeviceInformationFragment.java
@@ -20,6 +20,7 @@ package org.namelessrom.devicecontrol.fragments.device;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.SystemClock;
@@ -28,31 +29,28 @@ import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
+import android.text.format.DateFormat;
 
 import org.namelessrom.devicecontrol.Application;
+import org.namelessrom.devicecontrol.Logger;
 import org.namelessrom.devicecontrol.R;
+import org.namelessrom.devicecontrol.objects.CpuInfo;
+import org.namelessrom.devicecontrol.objects.KernelInfo;
 import org.namelessrom.devicecontrol.preferences.CustomPreference;
 import org.namelessrom.devicecontrol.utils.Utils;
 import org.namelessrom.devicecontrol.utils.constants.DeviceConstants;
 import org.namelessrom.devicecontrol.views.AttachPreferenceFragment;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Locale;
 
 public class DeviceInformationFragment extends AttachPreferenceFragment implements DeviceConstants {
 
-    private CustomPreference mPlatformVersion;
-    private CustomPreference mBuildId;
-    private CustomPreference mBuildType;
-    private CustomPreference mBuildTags;
-
-    private CustomPreference mVmLibrary;
-    private CustomPreference mVmVersion;
-
-    private CustomPreference mAndroidId;
-
-    private SensorManager mSensorManager;
+    private static final String KEY_PLATFORM_VERSION = "platform_version";
+    private static final String KEY_ANDROID_ID       = "android_id";
 
     private long[] mHits = new long[3];
 
@@ -62,54 +60,74 @@ public class DeviceInformationFragment extends AttachPreferenceFragment implemen
 
     @Override protected int getFragmentId() { return ID_DEVICE; }
 
-    @Override public void onCreate(Bundle savedInstanceState) {
+    @Override public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.device_information);
 
-        mSensorManager = (SensorManager) Application.get().getSystemService(Context.SENSOR_SERVICE);
+        final SensorManager sensorManager =
+                (SensorManager) Application.get().getSystemService(Context.SENSOR_SERVICE);
 
-        mPlatformVersion = (CustomPreference) findPreference("platform_version");
-        mPlatformVersion.setSummary(Build.VERSION.RELEASE);
+        // Platform
+        PreferenceCategory category = (PreferenceCategory) findPreference("platform");
 
-        mBuildId = (CustomPreference) findPreference("platform_id");
-        mBuildId.setSummary(Build.VERSION.CODENAME);
+        addPreference(category, KEY_PLATFORM_VERSION, R.string.version, Build.VERSION.RELEASE)
+                .setSelectable(true); // selectable because of the easter egg
+        addPreference(category, "platform_id", R.string.build_id, Build.DISPLAY);
+        addPreference(category, "platform_type", R.string.type,
+                Build.VERSION.CODENAME + " " + Build.TYPE);
+        addPreference(category, "platform_tags", R.string.tags, Build.TAGS);
+        addPreference(category, "platform_build_date", R.string.build_date, getDate(Build.TIME));
 
-        mBuildType = (CustomPreference) findPreference("platform_type");
-        mBuildType.setSummary(Build.TYPE);
+        // Runtime
+        category = (PreferenceCategory) findPreference("runtime");
 
-        mBuildTags = (CustomPreference) findPreference("platform_tags");
-        mBuildTags.setSummary(Build.TAGS);
-
-        // TODO: save / restore / check
-        mAndroidId = (CustomPreference) findPreference("android_id");
-        mAndroidId.setSummary(Utils.getAndroidId());
-
-        mVmLibrary = (CustomPreference) findPreference("vm_library");
-        final String library = Utils.getCommandResult("getprop persist.sys.dalvik.vm.lib", "-");
-        if (!TextUtils.equals(library, "-")) {
-            final String runtime = TextUtils.equals(library, "libdvm.so")
-                    ? "Dalvik"
-                    : TextUtils.equals(library, "libart.so") ? "ART" : "-";
-            mVmLibrary.setSummary(String.format("%s (%s)", runtime, library));
-        } else {
-            mVmLibrary.setSummary(library);
+        String summary = Utils.getCommandResult("getprop persist.sys.dalvik.vm.lib", "-");
+        if (!TextUtils.equals(summary, "-")) {
+            final String runtime = TextUtils.equals(summary, "libdvm.so")
+                    ? "Dalvik" : TextUtils.equals(summary, "libart.so") ? "ART" : "-";
+            summary = String.format("%s (%s)", runtime, summary);
         }
+        addPreference(category, "vm_library", R.string.type, summary);
+        addPreference(category, "vm_version", R.string.version,
+                System.getProperty("java.vm.version", "-"));
 
-        mVmVersion = (CustomPreference) findPreference("vm_version");
-        final String vmVersion = System.getProperty("java.vm.version", "-");
-        mVmVersion.setSummary(vmVersion);
+        // Device
+        category = (PreferenceCategory) findPreference("device_information");
 
-        PreferenceCategory category = (PreferenceCategory) findPreference("sensors");
+        // TODO: save / restore / check --> ANDROID ID
+        addPreference(category, KEY_ANDROID_ID, R.string.android_id, Utils.getAndroidId());
+        addPreference(category, "device_manufacturer", R.string.manufacturer, Build.MANUFACTURER);
+        addPreference(category, "device_model", R.string.model, Build.MODEL);
+        addPreference(category, "device_product", R.string.product, Build.PRODUCT);
+        addPreference(category, "device_board", R.string.board, Build.BOARD);
+        addPreference(category, "device_bootloader", R.string.bootloader, Build.BOOTLOADER);
+        addPreference(category, "device_radio_version", R.string.radio_version,
+                Build.getRadioVersion());
+
+        // Processor
+        category = (PreferenceCategory) findPreference("processor");
+
+        addPreference(category, "cpu_abi", R.string.cpu_abi, Build.CPU_ABI);
+        addPreference(category, "cpu_abi2", R.string.cpu_abi2, Build.CPU_ABI2);
+        new CpuInfoTask(category).execute();
+
+        // Kernel
+        category = (PreferenceCategory) findPreference("kernel");
+        new KernelInfoTask(category).execute();
+
+        // Sensors
+        category = (PreferenceCategory) findPreference("sensors");
 
         // we need an array list to be able to sort it, a normal list throws
         // java.lang.UnsupportedOperationException when sorting
         final ArrayList<Sensor> sensorList =
-                new ArrayList<Sensor>(mSensorManager.getSensorList(Sensor.TYPE_ALL));
+                new ArrayList<Sensor>(sensorManager.getSensorList(Sensor.TYPE_ALL));
 
         Collections.sort(sensorList, new SortIgnoreCase());
 
+        Preference preference;
         for (final Sensor s : sensorList) {
-            final CustomPreference preference = new CustomPreference(getActivity());
+            preference = new CustomPreference(getActivity());
             preference.setTitle(s.getName());
             preference.setSummary(s.getVendor());
             preference.setSelectable(false);
@@ -121,14 +139,25 @@ public class DeviceInformationFragment extends AttachPreferenceFragment implemen
         }
     }
 
+    private CustomPreference addPreference(final PreferenceCategory category, final String key,
+            final int titleResId, final String summary) {
+        final CustomPreference preference = new CustomPreference(getActivity());
+        preference.setKey(key);
+        preference.setTitle(titleResId);
+        preference.setSummary(TextUtils.isEmpty(summary) ? getString(R.string.unknown) : summary);
+        preference.setSelectable(false);
+        category.addPreference(preference);
+        return preference;
+    }
+
     @Override public boolean onPreferenceTreeClick(final PreferenceScreen preferenceScreen,
             @NonNull final Preference preference) {
-        if (mPlatformVersion == preference) {
+        if (TextUtils.equals(KEY_PLATFORM_VERSION, preference.getKey())) {
             System.arraycopy(mHits, 1, mHits, 0, mHits.length - 1);
             mHits[mHits.length - 1] = SystemClock.uptimeMillis();
             if (mHits[0] >= (SystemClock.uptimeMillis() - 500)) {
                 Utils.runRootCommand("am start android/com.android.internal.app.PlatLogoActivity");
-                mPlatformVersion.setSelectable(false);
+                preference.setSelectable(false);
             }
             return true;
         }
@@ -141,6 +170,62 @@ public class DeviceInformationFragment extends AttachPreferenceFragment implemen
             final String s1 = sensor1 != null ? sensor1.getName() : "";
             final String s2 = sensor2 != null ? sensor2.getName() : "";
             return s1.compareToIgnoreCase(s2);
+        }
+    }
+
+    private String getDate(final long time) {
+        final Calendar cal = Calendar.getInstance(Locale.ENGLISH);
+        cal.setTimeInMillis(time);
+        return DateFormat.format("dd-MM-yyyy", cal).toString();
+    }
+
+    private class CpuInfoTask extends AsyncTask<Void, Void, Boolean> {
+        private final CpuInfo            cpuInfo;
+        private final PreferenceCategory category;
+
+        public CpuInfoTask(final PreferenceCategory category) {
+            this.cpuInfo = new CpuInfo();
+            this.category = category;
+        }
+
+        @Override protected Boolean doInBackground(Void... voids) {
+            return cpuInfo.feedWithInformation();
+        }
+
+        @Override protected void onPostExecute(final Boolean success) {
+            if (success && category != null) {
+                Logger.i(this, cpuInfo.toString());
+                addPreference(category, "cpu_hardware", R.string.hardware, cpuInfo.hardware);
+                addPreference(category, "cpu_processor", R.string.processor, cpuInfo.processor);
+                addPreference(category, "cpu_features", R.string.features, cpuInfo.features);
+                addPreference(category, "cpu_bogomips", R.string.bogomips, cpuInfo.bogomips);
+            }
+        }
+    }
+
+    private class KernelInfoTask extends AsyncTask<Void, Void, Boolean> {
+        private final KernelInfo         kernelInfo;
+        private final PreferenceCategory category;
+
+        public KernelInfoTask(final PreferenceCategory category) {
+            this.kernelInfo = new KernelInfo();
+            this.category = category;
+        }
+
+        @Override protected Boolean doInBackground(Void... voids) {
+            return kernelInfo.feedWithInformation();
+        }
+
+        @Override protected void onPostExecute(final Boolean success) {
+            if (success && category != null) {
+                Logger.i(this, kernelInfo.toString());
+                addPreference(category, "kernel_version", R.string.version,
+                        String.format("%s %s", kernelInfo.version, kernelInfo.revision));
+                addPreference(category, "kernel_extras", R.string.extras, kernelInfo.extras);
+                addPreference(category, "kernel_gcc", R.string.toolchain, kernelInfo.gcc);
+                addPreference(category, "kernel_date", R.string.build_date, kernelInfo.date);
+                addPreference(category, "kernel_host", R.string.host, kernelInfo.host);
+            }
         }
     }
 
