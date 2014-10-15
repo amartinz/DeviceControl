f18e002c7b0f427c2b98c26e988b91f4f507985b
Evisceration <eviscerationls@gmail.com>
2014-10-15 19:21:14 +0200

    tests
    Change-Id: Ife19f21b0dd8635eb2c9d2c0f40c9a75599a8893

--- app/src/main/assets/js/files.js
+++ app/src/main/assets/js/files.js
@@ -11,10 +11,18 @@
         }
     });
 
-    app.controller('FileController', function ($http) {
+    app.directive('fileUpload', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'snippets/file_upload.html'
+        };
+    });
+
+    app.controller('FileController', function ($scope, $http) {
         var main = this;
         main.files = [];
         main.breadcrumbs = [];
+        main.currentLocation = {name: 'Home', path: '/files'};
 
         $http.get('/files').success(function (data) {
             main.files = data;
@@ -46,9 +54,50 @@
             path.forEach(function (entry) {
                 if (entry && entry.length !== 0) {
                     tmp += '/' + entry;
-                    main.breadcrumbs.push({name: entry, path: tmp});
+                    main.currentLocation = {name: entry, path: tmp};
+                    main.breadcrumbs.push(main.currentLocation);
                 }
             });
         };
+
+        addEventListener('load', this.load, false);
+
+        this.load = function () {
+            console.log('load');
+            'use strict';
+
+            var dropZone = angular.element(document.querySelector('#drop-zone'));
+            var uploadForm = angular.element(document.querySelector('#js-upload-form'));
+
+            var startUpload = function (files) {
+                console.log(files)
+            };
+
+            uploadForm.addEventListener('submit', function (e) {
+                var uploadFiles = document.getElementById('js-upload-files').files;
+                e.preventDefault();
+
+                startUpload(uploadFiles);
+            });
+
+            dropZone.ondrop = function (e) {
+                e.preventDefault();
+                this.className = 'upload-drop-zone';
+
+                startUpload(e.dataTransfer.files);
+            };
+
+            dropZone.ondragover = function () {
+                this.className = 'upload-drop-zone drop';
+                return false;
+            };
+
+            dropZone.ondragleave = function () {
+                this.className = 'upload-drop-zone';
+                return false;
+            };
+
+            alert('loaded!');
+        }
     });
-})();
+})(jQuery);
