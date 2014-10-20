6ea1d9061d39053f5ba9052ef0bf7d833c3f62dd
Evisceration <eviscerationls@gmail.com>
2014-10-20 16:25:43 +0200

    make webserver translatable
    Change-Id: I75ce0f699c340b55219748413e9076ed2586074d

--- app/src/main/assets/js/app.js
+++ app/src/main/assets/js/app.js
@@ -2,7 +2,7 @@
  * Created by alex on 13.10.14.
  */
 (function () {
-    var app = angular.module('interface', ['device', 'files']);
+    var app = angular.module('interface', ['device', 'files', 'pascalprecht.translate']);
 
     app.directive('mainContent', function () {
         return {
@@ -25,7 +25,12 @@
         }
     });
 
-    app.controller('NavigationBarController', function () {
+    app.controller('NavigationBarController', ['$translate', '$scope', function ($translate, $scope) {
+        $scope.changeLanguage = function (langKey) {
+            console.log(langKey);
+            $translate.use(langKey);
+        };
+
         this.mainTab = 1;
 
         this.setTab = function (selectedTab) {
@@ -35,8 +40,17 @@
         this.isSelected = function (checkTab) {
             return this.mainTab === checkTab;
         };
-    });
+    }]);
 
+    app.config(['$translateProvider', function ($translateProvider) {
+        $translateProvider.useStaticFilesLoader({
+            prefix: 'lang/locale-',
+            suffix: '.json'
+        });
+        //$translateProvider.determinePreferredLanguage();
+        $translateProvider.preferredLanguage('en');
+        $translateProvider.fallbackLanguage('en');
+    }]);
 })();
 
 $(document).ready(function () {
@@ -56,5 +70,16 @@ $(document).ready(function () {
     });
 
     $('#back-to-top').tooltip('show');
+
+    $(".dropdown").hover(
+        function () {
+            $('.dropdown-menu', this).stop(true, true).slideDown("fast");
+            $(this).toggleClass('open');
+        },
+        function () {
+            $('.dropdown-menu', this).stop(true, true).slideUp("fast");
+            $(this).toggleClass('open');
+        }
+    );
 
 });
