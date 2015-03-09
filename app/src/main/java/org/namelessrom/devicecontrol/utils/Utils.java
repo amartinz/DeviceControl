204ce75c3768dba16512aa7f1c3847169d5eb63e
Alexander Martinz <eviscerationls@gmail.com>
2015-03-09 20:19:15 +0100

    cpu: add frequency locking
      * just makes the cpu files unwriteable, simple but effective :P
    
    Change-Id: I44ca57fb6cfcf62e2da2a55927ce69bfbc2cc27a
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- app/src/main/java/org/namelessrom/devicecontrol/utils/Utils.java
+++ app/src/main/java/org/namelessrom/devicecontrol/utils/Utils.java
@@ -437,8 +437,12 @@ public class Utils implements DeviceConstants {
     }
 
     public static String getWriteCommand(final String path, final String value) {
-        return String.format("chmod 644 %s;\n", path) +
+        return String.format("busybox chmod 644 %s;\n", path) +
                 String.format("busybox echo \"%s\" > %s;\n", value, path);
+    }
+
+    public static String lockFile(String path) {
+        return String.format("busybox chmod 444 %s;", path);
     }
 
     public static void disableComponent(final String packageName, final String componentName) {
@@ -628,4 +632,5 @@ public class Utils implements DeviceConstants {
             closeable.close();
         } catch (IOException ignored) { }
     }
+
 }
