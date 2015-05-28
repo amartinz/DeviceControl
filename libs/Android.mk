0121ab9d12e4d218474040a8303d230cebee5a65
Alexander Martinz <eviscerationls@gmail.com>
2015-05-28 19:26:43 +0200

    Platform build: switch to gradle tasks
    Change-Id: Iecf9b3f1cd0c2ab1cbd882c5a5866e3978e6b24c
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- libs/Android.mk
+++ libs/Android.mk
@@ -1,9 +1,13 @@
 LOCAL_PATH:= $(call my-dir)
 
 #===================================================================
+# This jar contains api keys. just unzip it and you can even get the
+# keys with notepad. However policies state that i may not check the
+# api keys in plaintext in source control, that's why the jar exists
+#===================================================================
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := pollfish_4.0.3.jar
-LOCAL_MODULE := pollfish-sdk
+LOCAL_SRC_FILES := nameless-proprietary.jar
+LOCAL_MODULE := nameless-proprietary
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 LOCAL_MODULE_PATH := $(TARGET_OUT)/fake_packages/$(LOCAL_SRC_FILES)
@@ -11,13 +15,9 @@ include $(BUILD_PREBUILT)
 #===================================================================
 
 #===================================================================
-#This jar cointains api keys. just unzip it and you can even get the
-#keys with notepad. However policies state that i may not check the
-#api keys in plaintext in source control, thats why the jar is there
-#===================================================================
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := nameless-proprietary.jar
-LOCAL_MODULE := nameless-proprietary
+LOCAL_SRC_FILES := pollfish_4.0.3.jar
+LOCAL_MODULE := pollfish-sdk
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 LOCAL_MODULE_PATH := $(TARGET_OUT)/fake_packages/$(LOCAL_SRC_FILES)
