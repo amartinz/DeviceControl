de097fcdbb36c0adb6765f9c373cab1579b9904d
Alexander Martinz <eviscerationls@gmail.com>
2015-05-28 19:26:43 +0200

    Platform build: switch to gradle tasks
    Change-Id: Iecf9b3f1cd0c2ab1cbd882c5a5866e3978e6b24c
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,3 +1,19 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(LOCAL_PATH)/libs/Android.mk $(LOCAL_PATH)/app/src/main/Android.mk
+include $(CLEAR_VARS)
+
+LOCAL_GRADLE_SUBPROJECT := :app
+
+LOCAL_MODULE       := DeviceControl
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS  := optional
+
+LOCAL_PACKAGE_NAME      := DeviceControl
+LOCAL_CERTIFICATE       := platform
+LOCAL_PRIVILEGED_MODULE := true
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.pro
+
+include $(BUILD_GRADLE)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
---
+++ libs/README.md
@@ -0,0 +1,14 @@
+Prebuilt libraries
+=======
+
+
+nameless-proprietary.jar
+-------
+This jar contains api keys as policies state that i may not check-in the api keys in plaintext in source control.
+
+Just unzip it and you can even get the keys with any text editor.
+
+
+pollfish_4.0.3.jar
+-------
+See [Pollfish - Android Documentation](https://www.pollfish.com/android)
