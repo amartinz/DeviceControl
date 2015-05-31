6c0c29d73256742a4065fa776e8dbebd4e9c1c65
Alexander Martinz <eviscerationls@gmail.com>
2015-05-31 16:00:31 +0200

    gpu: convert to material preferences
    Change-Id: I7683e3782ce3cc85aa00b5106e5ac990c604a002
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- app/src/main/java/org/namelessrom/devicecontrol/modules/performance/GpuSettingsFragment.java
+++ app/src/main/java/org/namelessrom/devicecontrol/modules/performance/GpuSettingsFragment.java
@@ -17,35 +17,44 @@
  */
 package org.namelessrom.devicecontrol.modules.performance;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.preference.PreferenceCategory;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import org.namelessrom.devicecontrol.DeviceConstants;
 import org.namelessrom.devicecontrol.R;
 import org.namelessrom.devicecontrol.actions.ActionProcessor;
 import org.namelessrom.devicecontrol.hardware.GovernorUtils;
 import org.namelessrom.devicecontrol.hardware.GpuUtils;
-import org.namelessrom.devicecontrol.ui.preferences.CustomListPreference;
-import org.namelessrom.devicecontrol.ui.preferences.CustomPreference;
-import org.namelessrom.devicecontrol.ui.preferences.CustomTogglePreference;
-import org.namelessrom.devicecontrol.ui.views.AttachPreferenceFragment;
+import org.namelessrom.devicecontrol.theme.AppResources;
+import org.namelessrom.devicecontrol.ui.preferences.CustomPreferenceCategoryMaterial;
+import org.namelessrom.devicecontrol.ui.views.AttachMaterialPreferenceFragment;
 import org.namelessrom.devicecontrol.utils.Utils;
 
 import java.util.ArrayList;
 
-public class GpuSettingsFragment extends AttachPreferenceFragment implements
-        Preference.OnPreferenceChangeListener {
-    private PreferenceCategory mRoot;
+import alexander.martinz.libs.materialpreferences.MaterialListPreference;
+import alexander.martinz.libs.materialpreferences.MaterialPreference;
+import alexander.martinz.libs.materialpreferences.MaterialSwitchPreference;
 
-    private CustomListPreference mFreqMax = null;
-    private CustomListPreference mFreqMin = null;
-    private CustomListPreference mGpuGovernor = null;
-    private CustomTogglePreference m3dScaling = null;
+public class GpuSettingsFragment extends AttachMaterialPreferenceFragment implements MaterialPreference.MaterialPreferenceChangeListener {
+    private CustomPreferenceCategoryMaterial mCatGpu;
+
+    private MaterialListPreference mFreqMax = null;
+    private MaterialListPreference mFreqMin = null;
+    private MaterialListPreference mGpuGovernor = null;
+    private MaterialSwitchPreference m3dScaling = null;
 
     @Override protected int getFragmentId() {
         return DeviceConstants.ID_PERFORMANCE_GPU_SETTINGS;
+    }
+
+    @Override protected int getLayoutResourceId() {
+        return R.layout.preferences_gpu;
     }
 
     @Override public void onResume() {
@@ -53,15 +62,17 @@ public class GpuSettingsFragment extends AttachPreferenceFragment implements
         getGpu();
     }
 
-    @Override public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.gpu);
-        mRoot = (PreferenceCategory) getPreferenceScreen().findPreference("gpu");
+    @Override public View onCreateView(LayoutInflater inflater, ViewGroup root, Bundle savedState) {
+        final View view = super.onCreateView(inflater, root, savedState);
+        assert view != null;
 
-        final PreferenceCategory category = (PreferenceCategory) findPreference("gpu_opengl");
+        mCatGpu = (CustomPreferenceCategoryMaterial) view.findViewById(R.id.cat_gpu);
+
+        CustomPreferenceCategoryMaterial category =
+                (CustomPreferenceCategoryMaterial) view.findViewById(R.id.cat_gpu_opengl);
         if (GpuUtils.isOpenGLES20Supported()) {
             // our preference and string for storing gpu / opengl information
-            Preference infoPref;
+            MaterialPreference infoPref;
 
             final ArrayList<String> glesInformation = GpuUtils.getOpenGLESInformation();
 
@@ -69,21 +80,20 @@ public class GpuSettingsFragment extends AttachPreferenceFragment implements
             for (int i = 0; i < glesInformation.size(); i++) {
                 tmp = glesInformation.get(i);
                 if (!TextUtils.isEmpty(tmp)) {
-                    infoPref = new CustomPreference(getActivity());
-                    infoPref.setTitle(GpuUtils.GL_STRINGS[i]);
-                    infoPref.setSummary(tmp);
+                    infoPref = new MaterialPreference(getActivity());
+                    infoPref.init(getActivity());
                     category.addPreference(infoPref);
+                    infoPref.setTitle(getString(GpuUtils.GL_STRINGS[i]));
+                    infoPref.setSummary(tmp);
                 }
             }
         }
 
-        // if we are not supported or we could not add any preferences, remove it
-        if (category.getPreferenceCount() <= 0) {
-            getPreferenceScreen().removePreference(category);
-        }
+        return view;
     }
 
     private void getGpu() {
+        final Activity activity = getActivity();
         String tmp;
 
         if (Utils.fileExists(GpuUtils.get().getGpuBasePath())) {
@@ -104,18 +114,17 @@ public class GpuSettingsFragment extends AttachPreferenceFragment implements
                 }
 
                 if (mFreqMax != null) {
-                    mFreqMax.setValue(GpuUtils.fromMHz(tmp));
-                    mFreqMax.setSummary(tmp);
+                    mFreqMax.setValue(tmp);
                 } else {
-                    mFreqMax = new CustomListPreference(getActivity());
+                    mFreqMax = new MaterialListPreference(activity);
+                    mFreqMax.init(activity);
                     mFreqMax.setKey("pref_max_gpu");
-                    mFreqMax.setTitle(R.string.gpu_freq_max);
-                    mFreqMax.setEntries(gpuNames);
-                    mFreqMax.setEntryValues(frequencies);
-                    mFreqMax.setValue(GpuUtils.fromMHz(tmp));
-                    mFreqMax.setSummary(tmp);
+                    mFreqMax.setTitle(getString(R.string.gpu_freq_max));
+                    mFreqMax.setAdapter(mFreqMax.createAdapter(gpuNames, frequencies));
+                    mFreqMax.setValue(tmp);
+                    mCatGpu.addPreference(mFreqMax);
+                    mFreqMax.setSpinnerTextViewColor(AppResources.get().getAccentColor());
                     mFreqMax.setOnPreferenceChangeListener(this);
-                    mRoot.addPreference(mFreqMax);
                 }
             }
 
@@ -130,37 +139,35 @@ public class GpuSettingsFragment extends AttachPreferenceFragment implements
                 }
 
                 if (mFreqMin != null) {
-                    mFreqMin.setValue(GpuUtils.fromMHz(tmp));
-                    mFreqMin.setSummary(tmp);
+                    mFreqMin.setValue(tmp);
                 } else {
-                    mFreqMin = new CustomListPreference(getActivity());
+                    mFreqMin = new MaterialListPreference(activity);
+                    mFreqMin.init(activity);
                     mFreqMin.setKey("pref_min_gpu");
-                    mFreqMin.setTitle(R.string.gpu_freq_min);
-                    mFreqMin.setEntries(gpuNames);
-                    mFreqMin.setEntryValues(frequencies);
-                    mFreqMin.setValue(GpuUtils.fromMHz(tmp));
-                    mFreqMin.setSummary(tmp);
+                    mFreqMin.setTitle(getString(R.string.gpu_freq_min));
+                    mFreqMin.setAdapter(mFreqMin.createAdapter(gpuNames, frequencies));
+                    mFreqMin.setValue(tmp);
+                    mCatGpu.addPreference(mFreqMin);
+                    mFreqMin.setSpinnerTextViewColor(AppResources.get().getAccentColor());
                     mFreqMin.setOnPreferenceChangeListener(this);
-                    mRoot.addPreference(mFreqMin);
                 }
             }
 
             tmp = gpu.governor;
             if (!TextUtils.isEmpty(tmp) && GpuUtils.get().containsGov(tmp)) {
                 if (mGpuGovernor != null) {
-                    mGpuGovernor.setSummary(tmp);
                     mGpuGovernor.setValue(tmp);
                 } else {
                     final String[] gpuGovs = GovernorUtils.get().getAvailableGpuGovernors();
-                    mGpuGovernor = new CustomListPreference(getActivity());
+                    mGpuGovernor = new MaterialListPreference(activity);
+                    mGpuGovernor.init(activity);
                     mGpuGovernor.setKey("pref_gpu_gov");
-                    mGpuGovernor.setTitle(R.string.gpu_governor);
-                    mGpuGovernor.setEntries(gpuGovs);
-                    mGpuGovernor.setEntryValues(gpuGovs);
-                    mGpuGovernor.setSummary(tmp);
+                    mGpuGovernor.setTitle(getString(R.string.gpu_governor));
+                    mGpuGovernor.setAdapter(mGpuGovernor.createAdapter(gpuGovs, gpuGovs));
                     mGpuGovernor.setValue(tmp);
+                    mCatGpu.addPreference(mGpuGovernor);
+                    mGpuGovernor.setSpinnerTextViewColor(AppResources.get().getAccentColor());
                     mGpuGovernor.setOnPreferenceChangeListener(this);
-                    mRoot.addPreference(mGpuGovernor);
                 }
             }
         }
@@ -168,36 +175,41 @@ public class GpuSettingsFragment extends AttachPreferenceFragment implements
         if (Utils.fileExists(GpuUtils.FILE_3D_SCALING)) {
             if (m3dScaling == null) {
                 tmp = Utils.readOneLine(GpuUtils.FILE_3D_SCALING);
-                m3dScaling = new CustomTogglePreference(getActivity());
+                m3dScaling = new MaterialSwitchPreference(activity);
+                m3dScaling.init(activity);
                 m3dScaling.setKey("3d_scaling");
-                m3dScaling.setTitle(R.string.gpu_3d_scaling);
-                m3dScaling.setSummary(R.string.gpu_3d_scaling_summary);
-                m3dScaling.setChecked(tmp != null && tmp.equals("1"));
+                m3dScaling.setTitle(getString(R.string.gpu_3d_scaling));
+                m3dScaling.setSummary(getString(R.string.gpu_3d_scaling_summary));
+                m3dScaling.setChecked(Utils.isEnabled(tmp, false));
+                mCatGpu.addPreference(m3dScaling);
                 m3dScaling.setOnPreferenceChangeListener(this);
-                mRoot.addPreference(m3dScaling);
             }
         }
 
-        isSupported(mRoot, getActivity());
+        final View child = mCatGpu.getCardView().getChildAt(0);
+        if (child instanceof LinearLayout && ((LinearLayout) child).getChildCount() <= 1) {
+            MaterialPreference noSupportPref = new MaterialPreference(activity);
+            noSupportPref.init(activity);
+            mCatGpu.addPreference(noSupportPref);
+            noSupportPref.setTitle(getString(R.string.no_tweaks_available));
+            noSupportPref.setSummary(getString(R.string.no_tweaks_message));
+        }
     }
 
-    @Override public boolean onPreferenceChange(final Preference preference, final Object objVal) {
+    @Override public boolean onPreferenceChanged(MaterialPreference preference, Object objVal) {
         if (mFreqMax == preference) {
             final String value = String.valueOf(objVal);
             mFreqMax.setValue(value);
-            mFreqMax.setSummary(GpuUtils.toMhz(value));
             ActionProcessor.processAction(ActionProcessor.ACTION_GPU_FREQUENCY_MAX, value, true);
             return true;
         } else if (mFreqMin == preference) {
             final String value = String.valueOf(objVal);
             mFreqMin.setValue(value);
-            mFreqMin.setSummary(GpuUtils.toMhz(value));
             ActionProcessor.processAction(ActionProcessor.ACTION_GPU_FREQUENCY_MIN, value, true);
             return true;
         } else if (mGpuGovernor == preference) {
             final String value = String.valueOf(objVal);
             mGpuGovernor.setValue(value);
-            mGpuGovernor.setSummary(value);
             ActionProcessor.processAction(ActionProcessor.ACTION_GPU_GOVERNOR, value, true);
             return true;
         } else if (m3dScaling == preference) {
