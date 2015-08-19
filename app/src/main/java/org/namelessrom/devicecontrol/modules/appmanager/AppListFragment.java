ff07773d3ca055d2a0476c13349e40fedb3f9763
Alexander Martinz <eviscerationls@gmail.com>
2015-08-19 11:13:47 +0200

    appmanager: allow swiping again
      * swipe layout is gone, allow swiping again
      * also update BottomSheet module
    
    Change-Id: Ia887803eda76f21c3e4faa1ca75e715541723b31
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>

--- app/src/main/java/org/namelessrom/devicecontrol/modules/appmanager/AppListFragment.java
+++ app/src/main/java/org/namelessrom/devicecontrol/modules/appmanager/AppListFragment.java
@@ -17,14 +17,11 @@
  */
 package org.namelessrom.devicecontrol.modules.appmanager;
 
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.View;
 
 import org.namelessrom.devicecontrol.DeviceConstants;
 import org.namelessrom.devicecontrol.R;
 import org.namelessrom.devicecontrol.ui.views.AttachViewPagerFragment;
-import org.namelessrom.devicecontrol.ui.views.NoSwipeViewPager;
 
 import java.util.ArrayList;
 
@@ -48,11 +45,4 @@ public class AppListFragment extends AttachViewPagerFragment {
         return new ViewPagerAdapter(getChildFragmentManager(), fragments, titles);
     }
 
-    @Override public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        final NoSwipeViewPager viewPager = getViewPager();
-        if (viewPager != null) {
-            viewPager.setAllowSwipe(false);
-        }
-    }
 }
